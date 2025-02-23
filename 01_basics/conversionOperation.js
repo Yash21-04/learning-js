abf8209e88194b51674f52ab0993979f498a8d36
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-02-23 23:27:26 +0530

    learned about basic conversion in js

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,9 @@
+let score = 100
+//console.log(typeof score);
+let isLoggedIn = "Yash"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+let someNumber = 35;
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+
