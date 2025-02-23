538f4b93da0f6fb2d03f4fc9f257b04ab575ad3d
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-02-23 23:02:56 +0530

    just started

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+/* prefer not to use var bcoz of issue in block anf functional scope 
+*/
+const accountid = 1133432
+let accountEmail = "ajkhkdsh@gmail.com"
+var accountPassword = "12383"
+accountCity = "delhi"
+accountEmail = "hjgf@gmail.com"
+accountPassword = "0000"
+console.log(accountid)
+console.log(accountEmail)
+console.log(accountPassword)
+console.table([accountEmail,accountPassword,accountid])
