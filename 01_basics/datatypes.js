f6851fb28c3cf85339ba9171ac5cfba3f7bbb483
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-02-23 23:17:45 +0530

    learned about datatypes in js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3)   // we r using node js not browser
+console.log(3 + 3);
+console.log("Yash");
+
+let name = "Yash"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2^53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol =>unique
+
+//object 
+
+
+console.log(typeof undefined);
