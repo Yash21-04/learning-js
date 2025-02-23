5cf04a8c6b6fd6b078e9ca91bd848897bacfaa6e
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-02-23 23:45:40 +0530

    learned about incremental operator and conversion in js

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -5,5 +5,14 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanisLoggedIn);
 let someNumber = 35;
 let stringNumber = String(someNumber)
-console.log(typeof(stringNumber));
+// console.log(typeof(stringNumber));
+let str1 = "hello"
+let str2 = " Yash"
+let str3 = str1 + str2
+console.log(str3);
+let num = 13
+++num
+console.log(num);
+
+
 
