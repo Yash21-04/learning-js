e0326899c0cdfe02e382681ec955b31879e3b87d
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-02-24 18:31:43 +0530

    learned about stack and heap in JS

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,17 @@
+let myName = "Yash"
+let anotherName = myName
+anotherName = "Apna bhai"
+console.log(myName);
+
+console.log(anotherName);
+
+let user1 = {
+    email : "yash@gmail.com",
+    upi : "user1234Q@ibl"
+
+}
+let user2 = user1;
+user2.email = "ansh@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
+
