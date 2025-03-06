6d617ef06bdb18258fe0745f508fa2c9ea75c6b0
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-03-06 23:29:56 +0530

    learned about arrays

---
+++ 01_basics/array.js
@@ -0,0 +1,48 @@
+/*
+const myArray = [1,2,3,4,5,6]
+const Array1 = new Array(1,2,4,6,7,9)
+console.log(myArray);
+console.log(Array1);
+console.log(Array1.length);
+console.log(myArray[2]);
+*/
+
+
+// const Array2 = [1,2,5,3,4,8,70]
+/*
+console.log(Array2);
+console.log(Array2.join());
+console.log(Array2.indexOf(2));
+*/
+/*  Array2.unshift(145)
+console.log(Array2);
+Array2.shift()
+console.log(Array2);
+
+*/
+/*
+Array2.push(65)
+console.log(Array2);
+Array2.pop()
+console.log(Array2);
+*/
+
+const array3 = [1,2,3,4,7,5,6]
+console.log("A ", array3);
+const arr1 = array3.slice(1,4)
+console.log(arr1);
+
+
+
+const array4 = [1,2,3,4,7,5,6]
+console.log("B ",array4);
+const arr2 = array4.splice(1,4)
+console.log(arr2);
+
+
+
+
+
+
+
+
