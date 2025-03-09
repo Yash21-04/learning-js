32f556ac1699246d89d51e47f7113d4b3a3b927f
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-03-09 23:37:53 +0530

    learned about function with Object as argument.

--- 01_basics/function.js
+++ 01_basics/function.js
@@ -1,6 +1,42 @@
-function myname(){
-    console.log("yash");
+// function myname(){
+//     console.log("yash");
     
+// }
+
+// myname()
+
+// function multiplyTwonumber(number1,number2){
+//     // let result = number1*number2;
+//     return number1*number2;
+    
+// }
+
+// const Result = multiplyTwonumber(12,4)
+// console.log("Result : ",Result);
+
+// function logoutUserMessage(username){
+//     if(username ==undefined){
+//         console.log("Please enter Username");
+//         return
+//     }
+//     return `${username} just logged out.`
+// }
+
+// console.log(logoutUserMessage());
+// function number(...num1){
+//     return num1
+// }
+
+// console.log(number(220,300,1089));
+
+const student = {
+    Name : "Yash",
+    USN : "1DT23IS189",
+    Branch: "Ise"
 }
 
-myname()
+function StudentDetails(anyObject){
+    console.log(`Student name is ${anyObject.Name} , Usn is ${anyObject.USN}, and Branch is ${anyObject.Branch}`);
+    
+}
+StudentDetails(student)
