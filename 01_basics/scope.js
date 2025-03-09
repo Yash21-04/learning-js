6f84486f91cee1dcb30afe65d027335f656f5150
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-03-10 01:00:46 +0530

    learned scope in JS

--- 01_basics/scope.js
+++ 01_basics/scope.js
@@ -1,8 +1,18 @@
-const a = 300;
-if (true) {
-  let a = 120;
-  const b = 15;
-  console.log("Value of a inside if block : " + a); // local scope
-  console.log("value of b inside if block: " + b);
+// const a = 300;
+// if (true) {
+//   let a = 120;
+//   const b = 15;
+//   console.log("Value of a inside if block : " + a); // local scope
+//   console.log("value of b inside if block: " + b);
+// }
+// console.log(a); // global scope
+
+function first() {
+  const name = "Yash";
+  function second() {
+    const age = "19"; // this is the example of scope within scope.
+    console.log(name);
+  }
+  second();
 }
-console.log(a); // global scope
+first();
