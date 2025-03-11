31f43bf7725265fbfad3e5216ac5bf4d101d455a
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-03-12 00:54:00 +0530

    learned coditional statements

---
+++ 01_basics/conditional.js
@@ -0,0 +1,18 @@
+// const emptyObject = {
+//     username : "yash"
+// }
+
+// if(Object.keys(emptyObject).length===0){
+//     console.log("Object is empty");
+    
+// }
+// else {
+//     console.log("Object is not empty");
+    
+// 
+
+let score = 100;
+ (score>80) ? console.log("score is more than 80") : console.log("score is less than 80");
+ 
+ 
+ 
