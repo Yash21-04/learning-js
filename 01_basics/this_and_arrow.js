d052eff9e835fd63f0fed603e632875336cd4618
Yash21-04 <yashshaileshojha9915@gmail.com>
2025-03-11 21:19:48 +0530

    learned how to use arrow function.

--- 01_basics/this_and_arrow.js
+++ 01_basics/this_and_arrow.js
@@ -1,16 +1,25 @@
-const user = {
-    username: "Yash",
-    age : 19,
+// const user = {
+//     username: "Yash",
+//     age : 19,
     
-    welcomeMessage : function(){
-        console.log(`hello ${this.username},welcome to website.`);
-        console.log(this);
+//     welcomeMessage : function(){
+//         console.log(`hello ${this.username},welcome to website.`);
+//         console.log(this);
         
-    }
+//     }
     
-}
+// }
+// // user.welcomeMessage()
+// // user.username = "Aaru"
+// // user.welcomeMessage()
 // user.welcomeMessage()
-// user.username = "Aaru"
-// user.welcomeMessage()
-user.welcomeMessage()
 
+// const tea = () =>{
+//     let Username = "Yash"
+//     console.log(`${this.Username} drinking tea right now.`);
+//     //basically we can't use this function in arrow function.
+// }
+// tea()
+
+const multiply = (num1,num2) => num1*num2
+console.log(multiply(4,5));
